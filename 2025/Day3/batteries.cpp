27f50f8b2b79e515f4724ef00b97ca54ba6d02ee
hmkflash <hmkflash@gmail.com>
2026-01-15 16:03:43 -0500

    Finished part 2 of day 3

--- 2025/Day3/batteries.cpp
+++ 2025/Day3/batteries.cpp
@@ -10,12 +10,12 @@ ll partOne()
 	std::ofstream output ("logPart1.txt");
 	if (input.is_open() && output.is_open())
 	{
-		output << "Battery Bank\t\t\t\t\t\t\t\t\t\t\t\tBank Max\n"; 
+		output << "Battery Bank\n\tBank Max\n\n"; 
 		while (getline(input, line))
 		{
-			output << line << "\t";
-			std::string tensPlace = "0"; // Placeholder for the tensplace for the battery voltage.
-			std::string onesPlace = "0"; // Placeholder for the onesplace for the battery voltage.
+			output << line << std::endl;
+			std::string tensPlace; // Placeholder for the tensplace for the battery voltage.
+			std::string onesPlace; // Placeholder for the onesplace for the battery voltage.
 			ll curMax = 0;
 			for (ll i = 0; i < (ll) line.length() - 1; i++)
 			{
@@ -29,11 +29,61 @@ ll partOne()
 					}
 				}
 			}
-			output << curMax << std::endl;
+			output << "\t" << curMax << std::endl << std::endl;
 			sum += curMax;
 		}
+		input.close();
+		output.close();
 	}
 	return sum;
+}
+
+ll partTwo()
+{
+	sum = 0;
+	std::string line;
+	std::ifstream input ("input.txt");
+	std::ofstream output ("logPart2.txt");
+	if (input.is_open() && output.is_open())
+	{
+		output << "Battery Bank\n\tBank Max\n\n";
+		while (getline(input, line))
+		{
+			output << line << std::endl;
+			{
+				ll temp = ptGreedy(line);
+				output << "\t" << temp << std::endl << std::endl;
+				sum += temp;
+			}
+		}
+		input.close();
+		output.close();
+	}
+	return sum;
+}
+
+ll ptGreedy(std::string line)
+{
+	ll hd = 0; // Highest Digit is the highest or largest digit within the current iteration 
+	int hdic = -1; // The index at which the hd is found at the current iteration
+	int hdip = -1; // The index at which the hd was found at the last iteration
+	std::string numstr = ""; // The string which will contain the highest voltage.
+	for (int i = 11; i >= 0; i--)
+	{
+		for (ll j = hdip + 1; j < (ll) line.length() - i; j++)
+		{
+			ll curDigit = (ll) (line.at(j) - '0');
+			if (curDigit > hd)
+			{
+				hd = curDigit;
+				hdic = j;
+			}
+		}
+		numstr += std::to_string(hd);
+		hdip = hdic;
+		hd = 0;
+	}
+	return stoll(numstr);
 }
 
 int main(int argc, char* argv[])
@@ -47,13 +97,13 @@ int main(int argc, char* argv[])
 	else if (argc >= 2 && *argv[1] == '2')
 	{
 		std::cout << "Running just Part Two" << std::endl;
-		std::cout << "The total voltage: " << "Still in progress" << std::endl;
+		std::cout << "The total voltage: " << partTwo() << std::endl;
 		return 0;
 	}
 	else
 	{
 		std::cout << "The total voltage for Part One: " << partOne() << std::endl;
-		std::cout << "The total voltage for Part Two: " << "Still in progress" << std::endl;
+		std::cout << "The total voltage for Part Two: " << partTwo() << std::endl;
 		return 0;
 	}
 }
--- 2025/Day3/batteries.hpp
+++ 2025/Day3/batteries.hpp
@@ -4,5 +4,14 @@
 
 #define ll long long
 
+// Returns the sum of the highest voltages among battery banks as defined by part one.
+ll partOne();
+
+// Returns the sum of the highest voltages among battery banks as defined by part two.
+ll partTwo();
+
+// Returns the highest voltage of a single battery bank as defined by part two.
+ll ptGreedy(std::string line);
+
 // Takes in arguments to determine which parts to run.
 int main(int argc, char* argv[]);
