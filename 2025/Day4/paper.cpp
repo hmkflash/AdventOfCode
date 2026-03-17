665a34d950bf3680690de7777a03dcaa349c1ecf
hmkflash <hmkflash@gmail.com>
2026-03-17 19:03:40 -0400

    Day 4 is complete!

--- 2025/Day4/paper.cpp
+++ 2025/Day4/paper.cpp
@@ -1,6 +1,20 @@
+﻿/**
+ * @author Holden Kuempel
+ * 
+ * This program was developed to solve the 2025 Advent of Code Day 4, parts 1 and 2.
+ * 
+ * The challenge involves a warehouse of paper rolls. A roll cannot be cleared if it is adjacent to 4 or more 
+ * other rolls. In part one, only the rolls that can be immediately removed are. In part two, this process is 
+ * repeated until no additional rolls can be removed.
+ * 
+ * This program may take in an argument of 1 or 2. If it is provided it will only run that part of the challenge.
+ * Otherwise, it will run both parts.
+ * 
+ * To complete this challenge yourself go to: https://adventofcode.com/2025/day/4
+ */
 #include "paper.hpp"
 
-int count = 0;
+// dataSet is a vector of vectors containing the individual chars of the data.
 std::vector<std::vector<char>> dataSet;
 
 int main(int argc, char* argv[])
@@ -14,29 +28,58 @@ int main(int argc, char* argv[])
 	else if (argc >= 2 && *argv[1] == '2')
 	{
 		std::cout << "Running just Part Two" << std::endl;
-		std::cout << "???: " << std::endl;
+		std::cout << "Accessible rolls: " << partTwo() << std::endl;
 		return 0;
 	}
 	else
 	{
 		std::cout << "Accessible rolls for Part One: " << partOne() << std::endl;
-		std::cout << "???: " << std::endl;
+		std::cout << "Accessible rolld for Part Two: " << partTwo() << std::endl;
 		return 0;
 	}
 }
 
 int partOne()
 {
-	parseData();
-	if (dataSet.empty()) {return -1;}
-	std::ofstream output ("logPart1.txt");
+	parseData("input.txt");
+	return parseOneRound("logPart1.txt");
+}
+
+int partTwo()
+{
+	unsigned long long count = 0; // Total number of rolls removed.
+	parseData("input.txt");
+	std::string logRoot = "logPart2-";
+	int countOfCalls = 1; // Kept track of for the sake of log tracking.
+	std::string logPrefix = ".txt";
+	int updatedCount = parseOneRound(logRoot + std::to_string(countOfCalls) + logPrefix);
+	count += updatedCount; // updatedCount is the number of rolls removed after the most recent iteration.
+	while (updatedCount > 0) // Runs until nothing can be removed.
+	{
+		parseData(logRoot + std::to_string(countOfCalls) + logPrefix);
+		countOfCalls++;
+		updatedCount = parseOneRound(logRoot + std::to_string(countOfCalls) + logPrefix);
+		count += updatedCount;
+	}
+	return count;
+}
+
+// This function will iterate through the dataSet and count the number of rolls of paper that can be moved based 
+// on its curent state. The parameter log is the name of the log file generated, which will show an 'X' in each
+// spot that a roll was removed, a '.' in each spot that was initally lacking a roll, and a '@' in each spot 
+// where a roll couldn't be moved. 
+int parseOneRound(std::string log)
+{
+	unsigned long long count = 0;
+ 	if (dataSet.empty()) {return -1;}
+	std::ofstream output (log);
 	if (output.is_open())
 	{
 		for (int y = 0; y < (int) dataSet.size(); y++)
 		{
 			for (int x = 0; x < (int) dataSet.at(y).size(); x++)
 			{
-				if (dataSet.at(y).at(x) == '.') {output << "."; continue;}
+				if (dataSet.at(y).at(x) != '@') {output << "."; continue;}
 				if (x == 0)
 				{
 					if (y == 0)
@@ -169,12 +212,14 @@ int partOne()
 	return count;
 }
 
-void parseData()
+// dataSet copies the data of the inputName file into a format that can be more easily accessed.
+void parseData(std::string inputName)
 {
+	dataSet.clear();
 	if (dataSet.empty())
 	{
 		std::string line;
-		std::ifstream input ("input.txt");
+		std::ifstream input (inputName);
 		if (input.is_open())
 		{
 			while (getline(input, line))
--- 2025/Day4/paper.hpp
+++ 2025/Day4/paper.hpp
@@ -3,8 +3,17 @@
 #include <string>
 #include <vector>
 
+// Returns the number of rolls of paper that can be removed after one pass.
 int partOne();
 
-void parseData();
+// Returns the number of rolls of paper that can be removed in total.
+int partTwo();
 
+// Generates the vector off of a inputName.
+void parseData(std::string inputName);
+
+// Returns the number of rolls of paper that can be removed from one round of parsing.
+int parseOneRound(std::string log);
+
+// Takes in arguments to determine which parts to run.
 int main(int argc, char* arv[]);
