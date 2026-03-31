f0867de2da2d3e4f7fa61783a00957b267a21e63
hmkflash <hmkflash@gmail.com>
2026-03-31 15:22:08 -0400

    Made progress for Day 8 part 1

--- 2025/Day8/junction.cpp
+++ 2025/Day8/junction.cpp
@@ -20,18 +20,61 @@ void constructBoxes(std::string inputName) {
 	}
 }
 
-std::vector<std::pair<Box, Box>> findNClosestPairs3d(std::vector<Box> Boxes, int numOfPairs) {
-	std::sort(Boxes.begin(), Boxes.end(), [](Box a, Box b) {
-		return a.x < b.x;
-	});
-	std::vector<Box> tempLeft;
-	std::vector<Box> tempRight;
-	for (auto& it = Boxes.begin)
+std::vector<ClosePair> findNClosestPairs3d(std::vector<Box>::iterator first, std::vector<Box>::iterator last, size_t n, size_t numOfPairs) {
+	if (n == 2) {
+		std::vector<ClosePair> vTemp;
+		ClosePair pTemp;
+		pTemp.distance = abs(
+			((first+1)->x - first->x) * ((first+1)->x - first->x) +
+			((first+1)->y - first->y) * ((first+1)->y - first->y) +
+			((first+1)->z - first->z) * ((first+1)->z - first->z)
+		); 
+		// Distance calculate is techincally incorrect as it doesn't sqrt, but since no junction boxes are on 
+		// top of each other and input is in ints the minimum distance between two junctions is 1 and so even 
+		// if the distance is technically incorrect when comparing these distances to each other it is still 
+		// correct.
+		pTemp.a = *first;
+		pTemp.b = *(first+1);
+		vTemp.push_back(pTemp);
+		return vTemp;
+	}
+	std::vector<ClosePair> leftPart = findNClosestPairs3d(first, first + (n/2) + 1, (n/2) + 1, numOfPairs);
+	std::vector<ClosePair> rightPart = findNClosestPairs3d(first + (n/2), last, (n/2) + (n%2), numOfPairs);
+	std::vector<ClosePair> fullPart;
+	size_t l = 0;
+	size_t r = 0;
+	while (fullPart.size() < numOfPairs) {
+		if (l >= leftPart.size() && r >= rightPart.size()) {
+			break;
+		} else if (l >= leftPart.size()) {
+			fullPart.push_back(rightPart[r]);
+			r++;
+		} else if (r >= rightPart.size()) {
+			fullPart.push_back(leftPart[l]);
+			l++;
+		} else if (leftPart[l].distance <= rightPart[r].distance) {
+			fullPart.push_back(leftPart[l]);
+			l++;
+		} else {
+			fullPart.push_back(rightPart[r]);
+			r++;
+		}
+	}
+	return fullPart;
 }
 
 ull partOne(std::string inputName) {
 	constructBoxes(inputName);
-	findNClosestPairs3d(allBoxes, 1000);
+	// Sorts this segment of the data
+	std::sort(allBoxes.begin(), allBoxes.end(), [](Box a, Box b) {
+		return a.x < b.x;
+	});
+	std::vector<ClosePair> closestPairs = findNClosestPairs3d(allBoxes.begin(), allBoxes.end(), allBoxes.size(), 1000);
+	for (auto& elem : closestPairs) {
+		std::cerr << "Distance: " << elem.distance << ".\nPair A = {" << elem.a.x << ", " << elem.a.y << ", " << 
+				elem.a.z << "}.\nPair B = {" << elem.b.x << ", " << elem.b.y << ", " << elem.b.z << "}.\n\n";
+	}
+	std::cerr << "Num of pairs: " << closestPairs.size() << std::endl;
 	return 0;
 }
 
--- 2025/Day8/junction.hpp
+++ 2025/Day8/junction.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 #define ull unsigned long long
 
@@ -13,9 +14,16 @@ struct Box {
 	ull z;
 };
 
+// Thus structure represents the Boxes in a pair and the distance between them.
+struct ClosePair{
+	long double distance;
+	Box a;
+	Box b;
+};
+
 void constructBoxes(std::string inputName);
 
-std::vector<std::pair<Box, Box>> findNClosestPairs3d(std::vector<Box> Boxes, int numOfPairs);
+std::vector<ClosePair> findNClosestPairs3d(std::vector<Box>::iterator first, std::vector<Box>::iterator last, int numOfPairs);
 
 ull partOne(std::string inputName);
 
